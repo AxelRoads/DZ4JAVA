f584dfd8c8025fbaa37e2c8d733fe2631c7c8651
Alex Black <chernousov1987@gmail.com>
2022-12-04 21:46:46 +0500

    new commit

--- src/dz4task1.java
+++ src/dz4task1.java
@@ -1,9 +1,8 @@
-
 import java.util.*;
 
 /*
     Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
- */
+*/
 
 public class dz4task1 {
     public static void main(String[] args)
